63ad657b06f7b79cddb9535b626ce734f8bd48bd
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2025-04-27 07:47:18 +0200

    colorBarVGA: cleanup and moved to cc_toolchain.mk/nextpnr.mk

--- colorBarVGA/colorBarVGA.v
+++ colorBarVGA/colorBarVGA.v
@@ -7,10 +7,7 @@ module colorBarVGA (
 	output wire [3:0] o_g,
 	output wire [3:0] o_b,
 	output wire       o_vsync,
-	output wire       o_hsync,
-	output wire       o_clk,
-	output wire       o_rst,
-	output wire [7:0] o_led
+	output wire       o_hsync
 );
 
 wire clk_pix, lock;
@@ -28,8 +25,6 @@ pll pll_inst (
     .locked(lock)
 );
 `endif
-assign o_clk = 1'b0;//clk_pix;
-assign o_rst = 1'b0;//lock;
 wire   rst_s = ~lock;
 
 localparam
@@ -58,11 +53,5 @@ color_bar #(
 	.i_blank(~de_s),
 	.o_r(o_r), .o_g(o_g), .o_b(o_b)
 );
-
-assign o_led = 8'b0;
-//blinker #(.MAX(25_000_000)
-//) blink_inst (
-//	.i_clk(clk_pix), .i_rst(rst_s), .o_led(o_led)
-//);
 
 endmodule
