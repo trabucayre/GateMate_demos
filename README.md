05f3f40835c6ba0032b7955d45a66c9230188fea
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2025-10-04 09:23:53 +0200

    toolchain-sources-builder.sh: bash script to built the toolchain from source (yosys/prjpeppercorn/nextpnr)

--- README.md
+++ README.md
@@ -43,12 +43,56 @@ export CC_TOOL=/path/to/cc-toolchain-linux
 The easiest method is to use the [OSS CAD Suite](https://github.com/YosysHQ/oss-cad-suite-build/releases).
 
 1. Download and extract the latest release.
-2. Source the environment to add tools to your `\$PATH`:
+2. Source the environment to add tools to your `$PATH`:
 
 ```bash
 source /path/to/oss-cad-suite/environment
 ```
 
-#### Alternate Method (TBD)
+#### Alternate Method: toolchain-sources-build.sh
 
-You can also build **yosys**, **prjpeppercorn**, and **nextpnr** from source (instructions coming soon).
+You can also build [**yosys**](https://github.com/YosysHQ/yosys),
+[**prjpeppercorn**](https://github.com/YosysHQ/prjpeppercorn) and [**nextpnr**](https://github.com/YosysHQ/nextpnr) from source.
+
+This bash script provides an alternative to using *oss-cad-suite* for setting
+up the toolchain. It automates the process of downloading, building, and
+installing FPGA toolchain components manually.
+
+**Objectives**
+
+The script handles the following tasks:
+
+- checking and installing build dependencies
+- cloning, updating and checking out latest [gatemate-timings-latest](https://colognechip.com/downloads/gatemate-timings-latest.tar.gz)
+- building each specified tools
+- installing the resulting binaries into the */opt/gatemate* directory (default target path).
+
+**Usage**
+
+The script can be exceuted with or without arguments:
+
+- with no arguments or `all`: downloads, builds and installs all supported tools
+- with specific tool names (`yosys` and/or `nextpnr` and/or `prjpeppercorn`) only the specified tools will be downloaded, built, and installed.
+- with `INSTALL_PREFIX=CUSTOM_DIRECTORY` before the script (or exported): all tools will be installed in the specified directory instead
+  of */opt/gatemate*
+
+```bash
+[INSTALL_PREFIX=/somewhere] ./toolchain-sources-builder.sh [opts]
+```
+
+with:
+- `INSTALL_PREFIX` (optional): the install directory (*/opt/gatemate* when not specified)
+- `opts` (optional): may be `all` or any combinaisons of `nextpnr`, `yosys` and `prjpeppercorn` (`all` when not specified)
+
+**Environment setup**
+
+After installation, the script generate a file at *opt/gatemate/export.sh*,
+which can be sourced in your terminal to update the environment variables
+accordingly:
+
+```bash
+source /opt/gatemate/export.sh
+```
+
+This allows you to use the installed tools in your current shell session without manually modifying/adding:
+- `PATH`
---
+++ toolchain-sources-builder.sh
@@ -0,0 +1,207 @@
+#!/usr/bin/bash
+
+#
+# toolchain-sources-builder.sh
+#
+# Copyright (C) 2025  Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
+# SPDX-License-Identifier: MIT
+#
+
+# Install directory
+: ${INSTALL_PREFIX:=/opt/gatemate}
+
+# path where all repositories will be cloned
+WORK_DIR="$(pwd)/toolchain_build"
+
+# Option to use with cmake.
+CMAKE_OPTS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX"
+
+# nextpnr options.
+PEPPERCORN_PATH="$WORK_DIR/prjpeppercorn"
+
+# Yosys Dependencies.
+DEPENDENCIES="libreadline-dev zlib1g zlib1g-dev tcl-dev libffi-dev bison flex"
+# prjpeppercorn Dependencies.
+DEPENDENCIES="$DEPENDENCIES wget libboost-program-options-dev libboost-filesystem-dev libboost-thread-dev"
+
+# nextpnr Dependencies.
+DEPENDENCIES="$DEPENDENCIES libboost-iostreams-dev libboost-python-dev libeigen3-dev"
+
+check_dependencies() {
+	dep_install=""
+	for package in $DEPENDENCIES; do
+		ret=$(dpkg -l | grep $package)
+		if [[ "$ret" == "" ]]; then
+			#dep_install="$dep_install$package "
+			dep_install+="$package "
+		fi
+	done
+
+	if [[ $dep_install != "" ]]; then
+		echo "Missing package: $dep_install"
+		echo "sudo apt install $dep_install"
+		sudo apt install $dep_install
+	fi
+}
+
+git_clone_update() {
+	if [[ $# == 0 ]]; then
+		echo "wrong arguments"
+		return
+	fi
+
+	repo=$1
+	repo_dir=$WORK_DIR/$repo
+
+	if [ -d $repo_dir ]; then
+		pushd $repo_dir
+		git checkout .
+		git pull
+		git submodule update --init --recursive
+	else
+		case "$repo" in
+			yosys)
+				repo_url="https://github.com/YosysHQ/yosys.git";;
+			nextpnr)
+		 		repo_url="https://github.com/YosysHQ/nextpnr";;
+			prjpeppercorn)
+            	repo_url="https://github.com/YosysHQ/prjpeppercorn.git";;
+
+			*)
+				echo "Error: unknown repo $repo"
+				return;;
+		esac
+
+		git clone --recursive $repo_url $repo_dir
+   		pushd $repo_dir
+	fi
+
+   	popd
+}
+
+clean_repo() {
+	if [[ $# != 1 ]]; then
+		echo "wrong arguments"
+		return
+	fi
+
+	repo=$WORK_DIR/$1
+
+	pushd $repo
+	make clean
+	git clean -fd .
+	git clean -fX .
+	git clean -fx .
+	git checkout .
+	git clean -fd && git clean -fX && git clean -fx
+	popd
+}
+
+build_yosys() {
+	repo=$WORK_DIR/$1
+	pushd $repo
+	make -j$(nproc)
+	make install PREFIX=$INSTALL_PREFIX
+	popd
+}
+
+build_prjpeppercorn() {
+	repo=$WORK_DIR/$1
+	pushd $repo
+
+    sh delay.sh
+
+    mkdir -p libgm/build
+
+    pushd libgm/build
+    cmake $CMAKE_OPTS ../
+    make -j$(nproc)
+    make install
+    popd
+	popd
+}
+
+build_nextpnr() {
+	# apt install libboost-iostreams-dev libboost-thread-dev libboost-program-options-dev
+	# apt install libboost-python-dev libeigen3-dev
+	if [[ $# > 1 ]]; then
+		echo "Error: too many args"
+		return
+	fi
+
+	repo=$WORK_DIR/$1
+
+	CMD=". -B build -DUSE_OPENMP=ON -DHIMBAECHEL_UARCH=gatemate"
+	CMD="$CMD -DHIMBAECHEL_PEPPERCORN_PATH=$PEPPERCORN_PATH -DHIMBAECHEL_SPLIT=ON -DARCH=himbaechel"
+
+	pushd $repo
+
+	cmake $CMAKE_OPTS $CMD
+	cmake --build build
+	cmake --install build
+
+	popd 
+}
+
+# Check working directory. Create it when missing
+[ -d $WORK_DIR ] || mkdir -p $WORK_DIR
+
+# Check Target Directory. Create it when missing
+if [ ! -d $INSTALL_PREFIX ]; then
+	sudo mkdir -p $INSTALL_PREFIX
+	sudo chown -R $UID:$GROUPS $INSTALL_PREFIX
+fi
+
+# check if everything must be build or only one step.
+build_yosys="false"
+build_prjpeppercorn="false"
+build_nextpnr="false"
+
+# No arguments or all
+if [[ "$#" = 0 || $1 == "all" ]]; then
+	build_yosys="true"
+	build_prjpeppercorn="true"
+	build_nextpnr="true"
+else
+	# with arguments != all
+	# loop over the list
+	for tgt in $@; do
+		if [[ $tgt == "yosys" ]]; then
+			build_yosys="true"
+		fi
+		if [[ $tgt == "prjpeppercorn" ]]; then
+			build_prjpeppercorn="true"
+		fi
+		if [[ $tgt == "nextpnr" ]]; then
+			build_nextpnr="true"
+		fi
+	done
+fi
+
+# Check/Install Dependencies
+check_dependencies
+
+# YOSYS
+if [[ $build_yosys == "true" ]]; then
+	git_clone_update yosys
+	clean_repo yosys
+	build_yosys yosys
+fi
+
+# PRJPEPPERCORN
+if [[ $build_prjpeppercorn == "true" ]]; then
+	git_clone_update prjpeppercorn
+	clean_repo prjpeppercorn
+	build_prjpeppercorn prjpeppercorn
+fi
+
+# NEXTPNR
+if [[ $build_nextpnr == "true" ]]; then
+	git_clone_update nextpnr
+	clean_repo nextpnr
+	build_nextpnr nextpnr
+fi
+
+if [ ! -d $INSTALL_PREFIX/export.sh ]; then
+	echo "export PATH=$INSTALL_PREFIX/bin:\$PATH" > $INSTALL_PREFIX/export.sh
+fi
